961e9aeeb3cea7c6d0ab59d04acc3e0ff2636047
Salvador Villalon Jr <salvadorvillalon54@gmail.com>
2024-01-18 09:18:55 -0800

    testingwith try catch

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -4,34 +4,49 @@ const prisma = new PrismaClient();
 
 export async function getUsers(req: any, res: any, next: any) {
 	console.log('Inside get Users');
-	const usersDB = await prisma.user.findMany();
-	const users = usersDB.map((user) => {
-		const { username, id } = user;
-		return {
-			username,
-			_id: id
-		};
-	});
+	try {
+		const usersDB = await prisma.user.findMany();
 
-	res.status(200).json(users);
+		const users = usersDB.map((user) => {
+			const { username, id } = user;
+
+			return {
+				username,
+				_id: id
+			};
+		});
+
+		res.status(200).json(users);
+	} catch (error) {
+		res.status(500).json({
+			message: 'Error in getUsers',
+			error
+		});
+	}
 }
 
 export async function createNewUser(req: any, res: any, next: any) {
 	console.log('Inside createNewUser');
 	const { username: usernameInput } = req.body;
 	console.log({ usernameInput });
+	try {
+		const newUser = await prisma.user.create({
+			data: {
+				username: usernameInput
+			}
+		});
+		const { id, username } = newUser;
 
-	const newUser = await prisma.user.create({
-		data: {
-			username: usernameInput
-		}
-	});
-	const { id, username } = newUser;
-
-	console.log('New User create!');
-	console.log(newUser);
-	res.status(200).json({
-		_id: id,
-		username
-	});
+		console.log('New User create!');
+		console.log(newUser);
+		res.status(200).json({
+			_id: id,
+			username
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: 'Error in createUser',
+			error
+		});
+	}
 }
